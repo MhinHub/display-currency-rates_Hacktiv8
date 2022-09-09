34bdb345f000475eaf43ea34616ea4fce2532ecd
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-09-09 23:31:01 +0800

    add .env

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Card, Footer, Table } from "flowbite-react";
 import Head from "./components/Head";
 
 let country_list = ["IDR", "CAD", "JPY", "CHF", "EUR", "GBP"]
-let BASE_URL = "https://api.currencyfreaks.com/latest?apikey=410c97d2f6b34af8bacd492c14a404ba"
+let BASE_URL = `https://api.currencyfreaks.com/latest?apikey=${import.meta.env.VITE_API_KEY}&symbols=${country_list.join(",")}`
+
+// console.log(BASE_URL)
 
 export default function App() {
   const [currency, setCurrency] = useState(country_list);
@@ -29,10 +31,10 @@ export default function App() {
 
         setExchangeRate(country_list.map((country) => getExchangeRate(country)))
       })
-      .catch(console.log)
+      .catch(console.error)
   }, [])
 
-  console.log("rate", exchangeRate)
+  // console.log("rate", exchangeRate)
 
   useEffect(() => {
     setWeBuy(exchangeRate.map((rate) => parseFloat((rate) * 1.1).toFixed(2)))
@@ -48,11 +50,11 @@ export default function App() {
             <Table.Body className="divide-y">
 
               {currency.map((country, index) => (
-                <Table.Row >
-                  <Table.Cell>{country}</Table.Cell>
-                  <Table.Cell>{weBuy[index]}</Table.Cell>
-                  <Table.Cell>{exchangeRate[index]}</Table.Cell>
-                  <Table.Cell>{weSell[index]}</Table.Cell>
+                <Table.Row key={Math.random()}>
+                  <Table.Cell key={Math.random()}>{country}</Table.Cell>
+                  <Table.Cell key={Math.random()}>{weBuy[index]}</Table.Cell>
+                  <Table.Cell key={Math.random()}>{exchangeRate[index]}</Table.Cell>
+                  <Table.Cell key={Math.random()}>{weSell[index]}</Table.Cell>
                 </Table.Row>
               ))}
 
